9dff2658b0ea6b7956c914c15e2bd5ea3f7a1f8e
fibodevy <21068718+fibodevy@users.noreply.github.com>
2023-10-02 10:02:03 +0200

    Fixing Qooxdoo example (`webui_fn is not defined`)

--- examples/using Qooxdoo JS framework/qxexample.pas
+++ examples/using Qooxdoo JS framework/qxexample.pas
@@ -21,7 +21,7 @@ begin
   counter := 0;
   window := webui_new_window;
   webui_bind(window, 'Button1Click', @EventHandler);
-  content := '<!DOCTYPE html><html><head></head><body><script>function loadData(){fetch("index.html").then(response=>response.text()).then(content=>{document.open("text/html");document.write(content);document.close();console.log(content);}).catch(error=>{console.error(error);});}window.onload=loadData;</script></body></html>';
+  content := '<!DOCTYPE html><html><script src="/webui.js"></script><head></head><body><script>function loadData(){fetch("index.html").then(response=>response.text()).then(content=>{document.open("text/html");document.write(content);document.close();console.log(content);}).catch(error=>{console.error(error);});}window.onload=loadData;</script></body></html>';
   webui_show(window, content);
   webui_wait;
 end.
